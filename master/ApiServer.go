0a65fb49412fc75f5c250f4a92b557649b98070c
qgmac <qgmac@qq.com>
2018-10-24 20:55:40 +0800

    master架构搭建

---
+++ master/ApiServer.go
@@ -0,0 +1,55 @@
+package master
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+var(
+	//单例对象
+	G_apiServer *ApiServer
+)
+
+//任务的HTTP接口
+type ApiServer struct {
+	httpServer *http.Server
+}
+
+func handleJobSave(response http.ResponseWriter,r *http.Request){
+
+}
+
+//初始化服务
+func InitApiServer()(err error	) {
+	var(
+		mux *http.ServeMux
+		listener net.Listener
+		httpServer *http.Server
+	)
+	//配置路由
+	mux = http.NewServeMux()
+	mux.HandleFunc("/job/savd",handleJobSave)
+
+	//启动tcp监听
+	if listener ,err = net.Listen("tcp",":"+string(G_config.ApiPort));err!=nil {
+		return
+	}
+
+	//创建一个HTTP服务
+
+	httpServer = &http.Server{
+		ReadTimeout:time.Duration(G_config.ApiReadTiemout)*time.Millisecond,
+		WriteTimeout:time.Duration(G_config.ApiWriteTimeout)*time.Millisecond,
+		Handler:mux,
+	}
+
+	G_apiServer = &ApiServer{
+		httpServer:httpServer,
+	}
+
+	//启动服务端
+	go httpServer.Serve(listener)
+
+	return
+}
---
+++ master/Config.go
@@ -0,0 +1,36 @@
+package master
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config struct {
+	ApiPort string `json:"apiPort"`
+	ApiReadTiemout int `json:"apiReadTimeout"`
+	ApiWriteTimeout int `json:"apiWriteTimeout"`
+}
+
+var(
+	//单例
+	G_config *Config
+)
+func InitConfig(fielname string)(err error){
+	var(
+		content []byte
+		conf Config
+	)
+	//1.读取配置文件
+	if content,err = ioutil.ReadFile(fielname); err != nil {
+		return
+	}
+
+	//JOSN反序列化
+	if err = json.Unmarshal(content,&conf); err != nil {
+		return
+	}
+	G_config = &conf
+	fmt.Println(conf)
+	return
+}
